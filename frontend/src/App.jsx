7c3ece3d8c44c89e3b33e7de35e6f2dd15af9fb7
MEENAKSHYDARISH <meenakshyd23@gmail.com>
2025-12-05 15:59:53 +0530

    git push

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
-import Interviewer from './components/Interviewer'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import VideoInterviewer from "./interview/VideoInterviewer";
+import ReportView from "./interview/ReportView";
+import AvatarInterviewer from "./interview/AvatarInterviewer";
 
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
 
-export default function App(){
-return (
-<div className="min-h-screen flex items-center justify-center">
-<div className="w-full max-w-3xl p-6">
-<h1 className="text-2xl font-bold mb-4">AI Interviewer</h1>
-<Interviewer />
-</div>
-</div>
-)
+        {/* Home page */}
+        <Route path="/" element={<VideoInterviewer />} />
+
+        {/* Interview session */}
+        <Route path="/interview" element={<VideoInterviewer />} />
+
+        {/* Report page */}
+        <Route path="/report/:sessionId" element={<ReportView />} />
+
+        {/* Avatar test page */}
+        <Route path="/avatar" element={<AvatarInterviewer />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
+export default App;

6591a47ca2502dbcc2c917f7f7d2df4a98187941
Niveaann17 <niveaann32@gmail.com>
2025-12-12 14:10:44 +0530

    git push

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,11 @@
+import IntervuGoLogo from "./components/IntervuGoLogo";
+
+function App() {
+  return (
+    <div>
+      <IntervuGoLogo size={64} variant="full" animated />
+    </div>
+  );
+}
+
+export default App;

c31448b0d15eb4b8ee5b0d034bc79a12ac94056d
MEENAKSHYDARISH <meenakshyd23@gmail.com>
2026-01-20 18:58:50 +0530

    g

--- frontend/src/pages/student/StudentRoleSelect.jsx
+++ frontend/src/pages/student/StudentRoleSelect.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "./studentRoleSelect.css";
+
 
 export default function StudentRoleSelect() {
   const navigate = useNavigate();
